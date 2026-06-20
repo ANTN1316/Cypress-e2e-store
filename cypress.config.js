8e62d320340a2d879197eb2dc4bfe03adc301614
ANTN1316 <antoniorec2017@gmail.com>
2026-06-20 16:29:00 -0300

    primeira linha de código implementada

--- cypress.config.js
+++ cypress.config.js
@@ -1,1 +1,10 @@
+module.exports = {
+  projectId: 'epme66',
+  allowCypressEnv: false,
 
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -1,1 +1,20 @@
+describe('Funcionalidade: Login', () => {
+  
+  it('Deve realizar login com sucesso', () => {
+    // 1. Visitar a página de login
+    cy.visit('https://automationexercise.com/login' )
 
+    // 2. Preencher o email (Use um email que você cadastrou)
+    cy.get('[data-qa="login-email"]').type('seu_email@teste.com')
+
+    // 3. Preencher a senha
+    cy.get('[data-qa="login-password"]').type('123456')
+
+    // 4. Clicar no botão de login
+    cy.get('[data-qa="login-button"]').click()
+
+    // 5. Verificar se o login funcionou (Ex: procurar o botão de Logout)
+    cy.get('a[href="/logout"]').should('be.visible')
+  })
+
+})
