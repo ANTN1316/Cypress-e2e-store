34716581316ab64f9e3339148181f3ca0f315ab4
Antônio Geder <antoniorec2017@gmail.com>
2026-06-26 02:24:20 -0300

    Update LoginPage.js

--- cypress/pages/LoginPage.js
+++ cypress/pages/LoginPage.js
@@ -1,1 +1,49 @@
+class LoginPage {
 
+    elements = {
+
+        emailInput: () => cy.get('[data-qa="login-email"]'),
+
+        passwordInput: () => cy.get('[data-qa="login-password"]'),
+
+        loginButton: () => cy.get('[data-qa="login-button"]')
+
+    }
+
+    visit() {
+
+        cy.visit('/login')
+
+    }
+
+    fillEmail(email) {
+
+        this.elements.emailInput().type(email)
+
+    }
+
+    fillPassword(password) {
+
+        this.elements.passwordInput().type(password)
+
+    }
+
+    clickLogin() {
+
+        this.elements.loginButton().click()
+
+    }
+
+    login(email, password) {
+
+        this.fillEmail(email)
+
+        this.fillPassword(password)
+
+        this.clickLogin()
+
+    }
+
+}
+
+export default new LoginPage()
