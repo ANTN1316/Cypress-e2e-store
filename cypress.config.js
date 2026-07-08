785fa4575688d9bb64f7011e0988369b0fb3a1be
Antônio Geder <antoniorec2017@gmail.com>
2026-07-07 23:15:29 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
 module.exports = {
+
   projectId: 'epme66',
   allowCypressEnv: false,
 
   e2e: {
+
+    baseUrl: 'https://front.serverest.dev',
+
     setupNodeEvents(on, config) {
-      baseUrl: 'https://front.serverest.dev'
-    },
-  },
-};
+
+    }
+
+  }
+
+}
